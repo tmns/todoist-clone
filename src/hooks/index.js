078f6b7cae794ff43d2af7eecd670a3c4aa56f2a
sairim <spiri@protonmail.com>
2019-08-27 19:40:39 +0300

    implement useProjects custom hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -51,3 +51,28 @@ export const useTasks = selectedProject => {
 
   return { tasks, archivedTasks };
 };
+
+export const useProjects = () => {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("projects")
+      .where("userId", "==", "CNtExQfQ7vj4VxioKUui")
+      .orderBy("projectId")
+      .get()
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(project => ({
+           ...project.data(),
+           docId: project.id 
+        }))
+
+        if (JSON.stringify(allProjects) !== JSON.stringify(projects)) {
+          setProjects(allProjects);
+        }
+      });
+  }, [projects]);
+
+  return { projects, setProjects };
+};
