bcf299b3a947648d536a2112345205005dc509b9
sairim <spiri@protonmail.com>
2019-08-27 16:53:53 +0300

    draft up basic header and sidebar

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Sidebar = () => (
+  <div className="sidebar" data-testid="sidebar">
+    <ul className="sidebar__generic">
+      <li>Inbox</li>
+      <li>Today</li>
+      <li>Next 7 days</li>
+    </ul>
+  </div>
+);
