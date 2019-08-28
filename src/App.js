0d2952aa08f69fd465c599ca9737f6f280a8b69e
sairim <spiri@protonmail.com>
2019-08-28 12:31:59 +0300

    implement projects context

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { Header } from './components/layout/Header';
 import { Content } from './components/layout/Content';
+import { ProjectsProvider, SelectedProjectsProvider } from './context';
 
 export const App = () => (
-  <div className="App">
-    <Header />
-    <Content />
-  </div>
+  <SelectedProjectsProvider>
+    <ProjectsProvider>
+      <div className="App">
+        <Header />
+        <Content />
+      </div>
+    </ProjectsProvider>
+  </SelectedProjectsProvider>
 );
