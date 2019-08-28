8ff1f067b0430a557711bbf8f1850f113f713772
sairim <spiri@protonmail.com>
2019-08-28 17:39:25 +0300

    build out further the Tasks component and add styling

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import { Checkbox } from './Checkbox';
-import { useTasks } from '../hooks';
+import { Checkbox } from "./Checkbox";
+import { useTasks } from "../hooks";
+import { collatedTasks } from "../constants";
+import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
+import { useSelectedProjectValue, useProjectsValue } from "../context";
 
 export const Tasks = () => {
-  const { tasks } = useTasks("1");
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
 
-  let projectName = '';
+  let projectName = "";
+
+  if (
+    projects &&
+    projects.length > 0 &&
+    selectedProject &&
+    !collatedTasksExist(selectedProject)
+  ) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
+
+  if (collatedTasksExist(selectedProject) && selectedProject) {
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: Todoist`;
+  });
 
   return (
     <div className="tasks" data-testid="tasks">
@@ -15,11 +37,11 @@ export const Tasks = () => {
       <ul className="tasks__list">
         {tasks.map(task => (
           <li key={task.id}>
-            <Checkbox id={task.id} />
+            <Checkbox id={task.id} taskDesc={task.task}/>
             <span>{task.task}</span>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
