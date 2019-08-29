d63d5f131ed97d41af7aeeb13404ac4f66a843cd
sairim <spiri@protonmail.com>
2019-08-29 11:58:29 +0300

    implement draft of AddTask component

---
+++ src/components/AddTask.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { FaRegListAlt, FaRegCalendarAlt } from "react-icons/fa";
+import moment from "moment";
+import { firebase } from "../firebase";
+import { useSelectedProjectValue } from "../context";
+
+export const AddTask = ({
+  showAddTaskMain = true,
+  shouldShowMain = false,
+  showQuickAddTask,
+  setShowQuickAddTask
+}) => {
+  const [task, setTask] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+  const [project, setProject] = useState("");
+  const [showMain, setShowMain] = useState(shouldShowMain);
+  const [showProjectOverlay, setShowProjectOverlay] = useState(false);
+  const [showTaskDate, setShowTaskDate] = useState(false);
+
+  const { selectedProject } = useSelectedProjectValue();
+
+  const addTask = () => {
+    const projectId = project || selectedProject;
+    let collatedDate = "";
+
+    if (projectId === "TODAY") {
+      collatedDate = moment().format("DD/MM/YYYY");
+    } else if (projectId === "NEXT_7") {
+      collatedDate = moment()
+        .add(7, "days")
+        .format("DD/MM/YYYY");
+    }
+
+    return (
+      task &&
+      projectId &&
+      firebase
+        .firestore()
+        .collection("tasks")
+        .add({
+          archived: false,
+          projectId,
+          task,
+          date: collatedDate || taskDate,
+          userId: "CNtExQfQ7vj4VxioKUui"
+        })
+        .then(() => {
+          setTask("");
+          setProject("");
+          setShowMain("");
+          setShowProjectOverlay(false);
+        })
+    );
+  };
+
+  return (
+    <div
+      className={showQuickAddTask ? 'add-task add-task__overlay' : 'add-task'}
+      data-testid="add-task-comp"
+    >
+      {showAddTaskMain && (
+        <div
+          className="add-task__shallow"
+          data-testid="show-main-action"
+          onClick={() => setShowMain(!showMain)}
+        >
+          <span className="add-task__plus">+</span>
+          <span className="add-task__text">Add Task</span>
+        </div>
+      )}
+    </div>
+  )
+};
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -5,6 +5,7 @@ import { useTasks } from "../hooks";
 import { collatedTasks } from "../constants";
 import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
 import { useSelectedProjectValue, useProjectsValue } from "../context";
+import { AddTask } from "./AddTask";
 
 export const Tasks = () => {
   const { selectedProject } = useSelectedProjectValue();
@@ -42,6 +43,8 @@ export const Tasks = () => {
           </li>
         ))}
       </ul>
+
+      <AddTask />
     </div>
   );
 };
