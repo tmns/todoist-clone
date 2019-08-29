d43afffd7c0d1575e66d8c6ffdeb297817d1c6eb
sairim <spiri@protonmail.com>
2019-08-29 11:40:30 +0300

    fix bug in AddProject and add styling

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { firebase } from "firebase";
+import { firebase } from "../firebase";
 import { generatePushId } from "../helpers";
 import { useProjectsValue } from "../context";
 
