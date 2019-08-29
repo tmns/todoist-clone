3efcdc0c67c523f347ecd6bf9c947b5ff4a09db1
sairim <spiri@protonmail.com>
2019-08-29 13:17:01 +0300

    implement dark mode and quick add buttons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPizzaSlice } from "react-icons/fa";
+import { AddTask } from "../AddTask";
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
   return (
     <header className="header" data-testid="header">
       <nav>
@@ -10,14 +14,43 @@ export const Header = () => {
         </div>
         <div className="settings">
           <ul>
-            <li data-testid="quick-add-task-action" className="settings__add">
-              +
+            <li className="settings__add">
+              <button
+                data-testid="quick-add-task-action"
+                aria-label="Quick add task"
+                type="button"
+                onClick={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}
+                onKeyDown={() => {
+                  setShowQuickAddTask(true);
+                  setShouldShowMain(true);
+                }}
+              >
+                +
+              </button>
             </li>
-            <li data-testid="dark-mode-action" className="settings__dark-mode">
-              <FaPizzaSlice />
+            <li className="settings__darkmode">
+              <button
+                data-testid="dark-mode-action"
+                aria-label="Darkmode on/off"
+                type="button"
+                onClick={() => setDarkMode(!darkMode)}
+                onKeyDown={() => setDarkMode(!darkMode)}
+              >
+                <FaPizzaSlice />
+              </button>
             </li>
           </ul>
         </div>
+
+        <AddTask
+          showAddTaskMain={false}
+          shouldShowMain={shouldShowMain}
+          showQuickAddTask={showQuickAddTask}
+          setShowQuickAddTask={setShowQuickAddTask}
+        />
       </nav>
     </header>
   );
