5e0e013c3762badfea73bb2803e73f5e6f047b80
sairim <spiri@protonmail.com>
2019-09-01 15:13:12 +0300

    add checkbox tests

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -16,3 +16,30 @@ jest.mock("../firebase", () => ({
     }))
   }
 }));
+
+describe("<Checkbox />", () => {
+  describe("Success", () => {
+    it("renders the task checkbox", () => {
+      const { queryByTestId } = render(
+        <Checkbox id="1" taskDesc="test task description" />
+      );
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+    });
+
+    it("renders the task checkbox and accepts a click", () => {
+      const { queryByTestId } = render(
+        <Checkbox id="1" taskDesc="test task description" />
+      );
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+      fireEvent.click(queryByTestId("checkbox-action"));
+    });
+
+    it("renders the task checkbox and accepts a key press", () => {
+      const { queryByTestId } = render(
+        <Checkbox id="1" taskDesc="test task description" />
+      );
+      expect(queryByTestId("checkbox-action")).toBeTruthy();
+      fireEvent.keyPress(queryByTestId("checkbox-action"));
+    });
+  });
+});
